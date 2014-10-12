ad00c65dd975e0831698c65acb883410922b427a
shockham <samuel.hockham@gmail.com>
2014-10-12 19:40:21 +0100

    more rusty query stringing

--- src/app.rs
+++ src/app.rs
@@ -14,27 +14,31 @@ impl<'a> App<'a>  {
         }
     }
     
-    fn handle_client(&self, stream: &'a mut BufferedStream<TcpStream>) -> () {
+    fn handle_client<'a>(&self, stream: &'a mut BufferedStream<TcpStream>) -> () {
             let request:String = match stream.read_line() {
                 Err(e) => { format!("error: {}", e) }
                 Ok(stream_output) => { stream_output }
             };
             println!("{}", request);
             
-            let request_parts: Vec<&str> = request.as_slice().split(' ').collect();
+            let clone_slice = request.as_slice();
+            let request_parts: Vec<&str> = clone_slice.split(' ').collect();
             let full_path: &str = request_parts[1];
             
             let split_path: Vec<&str> = full_path.split('?').collect();
-
             let route = split_path[0];
             let query_string = match split_path.len() {
                1  => "",
                 _ => split_path[1]
             };
 
-            //let callback = self.routes.find(&route).unwrap();
-            //let content = callback();
-            let content = format!("<h1>TEST</h1><p>route:{}</p><p>query string:{}</p>",route,query_string);
+            let route_string = String::from_str(route);
+            println!("{}", route_string.as_slice());
+
+            //let callback: fn() -> &'static str = *self.routes.find(&route).unwrap();
+            let callback: fn() -> &'static str = *self.routes.find(&("/")).unwrap();
+            let content = callback();
+            //let content = format!("<h1>TEST</h1><p>route:{}</p><p>query string:{}</p>",route,query_string);
 
             let with_headers = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\ncontent-length: {}\r\n\r\n{}",content.len(),content);
 
@@ -51,10 +55,12 @@ impl<'a> App<'a>  {
         for stream in acceptor.incoming() {
             match stream {
                 Err(e) => { println!("error: {}", e) }
-                Ok(stream) => {//spawn(proc() {
-                    let mut buf_stream = BufferedStream::new(stream);
-                    self.handle_client(&mut buf_stream);
-                }//)
+                Ok(stream) => {
+                    //spawn(proc() {
+                        let mut buf_stream = BufferedStream::new(stream);
+                        self.handle_client(&mut buf_stream);
+                    //})
+                }
             }
         }
     }
