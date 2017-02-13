0d0d395f5a1764fc95e89c7cea90564705e266e4
shockham <samuel.hockham@gmail.com>
2017-02-13 16:45:01 +0000

    refactored/tidied up the code and added a Response type

--- examples/main.rs
+++ examples/main.rs
@@ -1,31 +1,31 @@
-#![feature(convert)]
-
 extern crate temple;
-extern crate banana_rs;
+extern crate banana;
 
 use temple::{html, elem};
-use banana_rs::{App, Request};
+use banana::{App, Request, Response};
 
 fn main() -> () {
     let mut a = App::new();
 
-    fn this_handler(_:Request) -> String{
-        "TESTTESTTEST".to_string()
+    fn this_handler(_:Request) -> Response {
+        Response::ok_html("TESTTESTTEST".to_string())
     }
 
-    fn another_handler(req:Request) -> String{
+    fn another_handler(req:Request) -> Response {
         let name:String = match req.query_string.get(&"name".to_string()) {
             Some(n) => n.clone(),
             None => "anonymous".to_string(),
         };
 
-        html("test",
-            elem("h1", "title", "Hello!".to_string()) +
-            elem("div", "container",
-                elem("p", "", req.method) +
-                elem("p", "", name).as_str()
-            ).as_str()
-        )
+        Response::ok_html(
+            html("test",
+                 elem("h1", "title", "Hello!".to_string()) +
+                 elem("div", "container",
+                      elem("p", "", req.method) +
+                      elem("p", "", name).as_str()
+                     ).as_str()
+                )
+            )
     }
 
     a.routes.insert("^/$", this_handler); 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(convert)]
-
 extern crate regex;
 extern crate bufstream;
 
@@ -14,13 +12,51 @@ use std::sync::{Arc, Mutex};
 use std::str;
 
 pub struct App {
-    pub routes: HashMap<&'static str,fn(req:Request) -> String>,
+    pub routes: HashMap<&'static str,fn(req:Request) -> Response>,
 }
 
 pub struct Request {
     pub method: String,
     pub query_string: HashMap<String, String>,
     pub route: String,
+}
+
+pub enum ResponseCode {
+    Ok,
+    NotFound,
+}
+
+fn response_code_str(code:&ResponseCode) -> &'static str {
+    match *code {
+        ResponseCode::Ok => "200 OK",
+        ResponseCode::NotFound => "404 Not Found",
+    }
+}
+
+pub struct Response {
+    pub code: ResponseCode,
+    pub content: String,
+    pub mimetype: &'static str,
+}
+
+impl Response {
+    pub fn ok_html(content: String) -> Response {
+        Response {
+            code: ResponseCode::Ok,
+            content: content,
+            mimetype: "text/plain; charset=utf-8",
+        }
+    }
+}
+
+impl Response {
+    pub fn create(&self) -> String {
+        format!("HTTP/1.1 {}\r\nContent-Type: {}\r\ncontent-length: {}\r\n\r\n{}", 
+                response_code_str(&self.code),
+                self.mimetype,
+                self.content.len(),
+                self.content)
+    }
 }
 
 impl Clone for App{
@@ -38,22 +74,7 @@ impl App  {
         }
     }
 
-    fn process_request(request:String) -> Request {
-        let req_re = match Regex::new("(?P<type>[A-Z^']+) (?P<route>[^']+) HTTP/(?P<http>[^']+)"){
-            Ok(re) => re,
-            Err(err) => panic!("{}", err),
-        };
-        let caps = req_re.captures(request.as_str()).unwrap();
-        let full_path: &str = caps.name("route").unwrap();
-        let req_type: &str = caps.name("type").unwrap();
-
-        let split_path: Vec<&str> = full_path.split('?').collect();
-        let route = split_path[0];
-        let query_string = match split_path.len() {
-            1  => "",
-            _ => split_path[1]
-        };
-
+    fn create_query_map(query_string:&str) -> HashMap<String, String> {
         let query_pairs: Vec<&str> = query_string.split('&').collect();
         let mut query_map: HashMap<String, String> = HashMap::new();
         for pairs in query_pairs.iter() {
@@ -64,50 +85,70 @@ impl App  {
             query_map.insert(split_pairs[0].to_string(), split_pairs[1].to_string());
         }
 
+        query_map
+    }
+
+    fn process_request(request:String) -> Request {
+        let req_re = Regex::new("(?P<type>[A-Z^']+) (?P<route>[^']+) HTTP/(?P<http>[^']+)").unwrap();
+        let (full_path, req_type) = match req_re.captures(request.as_str()) {
+            Some(caps) => {
+                let full_path = caps.name("route").unwrap_or("/");
+                let req_type = caps.name("type").unwrap_or("GET");
+                (full_path, req_type)
+            },
+            None => {
+                println!("falling back to default\nreq:{}", request);
+                ("/", "GET")
+            }
+        };
+
+        let split_path: Vec<&str> = full_path.split('?').collect();
+        let route = split_path[0];
+        let query_string = match split_path.len() {
+            1  => "",
+            _ => split_path[1]
+        };
+
         Request {
             method: req_type.to_string(),
-            query_string: query_map.clone(),
+            query_string: App::create_query_map(query_string),
             route: route.to_string(),
         }
     }
 
-    fn handle_client(&self, stream: &mut BufStream<TcpStream>) -> () { 
+    fn route(&self, req:Request) -> Response {
+        for (r, callback) in self.routes.iter() {
+            let re = Regex::new(*r).unwrap();
+            if re.is_match(req.route.as_str()) {
+                let call_func = *callback;
+                return call_func(req);
+            }
+        }
+        
+        Response {
+            code: ResponseCode::NotFound,
+            content: "NOT FOUND".to_string(),
+            mimetype: "text/plain; charset=utf-8",
+        }
+    }
+
+    fn handle_client(&self, stream: &mut BufStream<TcpStream>) { 
         let mut byte_req: [u8; 1024] = [0; 1024];
         let _ = stream.read(&mut byte_req).unwrap();
 
         let request:String = str::from_utf8(&byte_req).unwrap().to_string();
         let req:Request = App::process_request(request); 
 
-        let mut content = "Route does not exist".to_string();
-        for (r, callback) in self.routes.iter() {
-            let re = match Regex::new(*r){
-                Ok(re) => re,
-                Err(err) => panic!("{}", err),
-            };
-            let matched = re.is_match(req.route.as_str());
-            if matched {
-                let call_func = *callback;
-                content = call_func(req);
-                break;
-            }
-        }
+        let content = self.route(req);
 
-        let with_headers = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\ncontent-length: {}\r\n\r\n{}",content.len(),content);
 
-        match stream.write_all(with_headers.as_str().as_bytes()){
-            Ok(_) => return,
-            Err(e) => panic!("{}", e),
-        }
+        stream.write_all(content.create().as_bytes()).unwrap();
     }
 
     pub fn run(&self, address:&str) -> () {
-        let worker_no = 4;
+        const WORKER_NO:i32 = 8;
 
-        let listener = match TcpListener::bind(address){
-            Ok(acc) => acc,
-            Err(e) => panic!("{}", e),
-        };
-
+        let listener = TcpListener::bind(address).unwrap();
         println!("||Starting server||\nhttp://{}", address);
 
         //vec of the incoming streams
@@ -115,7 +156,7 @@ impl App  {
         let data = Arc::new(Mutex::new(streams));
 
         //spawn some worker threads
-        for _ in 0..worker_no {
+        for _ in 0..WORKER_NO {
             let data = data.clone();
             let l_app = self.clone();
             thread::spawn(move || {
